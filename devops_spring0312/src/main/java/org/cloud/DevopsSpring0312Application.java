d846773f092b0b725b4b19348f100495dabaa7f4
haeing0v0 <sm134884@gmail.com>
2026-04-15 11:52:03 +0900

    04/15 eclipse, vscode project add

---
+++ devops_spring0312/src/main/java/org/cloud/DevopsSpring0312Application.java
@@ -0,0 +1,13 @@
+package org.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsSpring0312Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsSpring0312Application.class, args);
+	}
+
+}
